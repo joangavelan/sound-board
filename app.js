fbcb30bcd9e937fb2dbcd3209c0e3152c9e6af17
Johan Gavelan <johangavelan@gmail.com>
2021-01-23 22:01:11 -0500

    project done

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('button');
+const container = document.querySelector('.container');
+
+const audio = new Audio();
+
+const playSound = (name) => {
+    audio.src = `./audio/${name}.mp3`
+    audio.play();
+}
 
 buttons.forEach(button => {
     button.addEventListener('click', function() {
-        const img = this.dataset.img;
+        const image = this.dataset.img;
+        const name = this.textContent;
 
-        document.body.style.background = `url(images/${img}.jpg)`
+        container.style.background = `url(images/${image}.jpg)`;
+        playSound(name);
+        
+        buttons.forEach(button => {
+            button.disabled = true;
+            setTimeout(() => {
+                button.disabled = false;
+            }, 1700);
+        })
     })
 })
